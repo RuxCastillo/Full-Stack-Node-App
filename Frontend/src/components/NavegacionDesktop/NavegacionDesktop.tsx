941230ad111ed5aaab86a30ae90f071033aea466
RuxCastillo <rubenldc1412@gmail.com>
2025-04-03 20:19:39 -0600

    search ya funciona como componente individual pero no funciona en full screen mode

--- Frontend/src/components/NavegacionDesktop/NavegacionDesktop.tsx
+++ Frontend/src/components/NavegacionDesktop/NavegacionDesktop.tsx
@@ -1,19 +1,48 @@
+import { useState } from 'react';
 import searchIcon from '../../assets/images/icon-search.svg';
 import settingsIcon from '../../assets/images/icon-settings.svg';
 import ElementoListaDeNotas from '../ListaDeNotas/ElementoListaDeNotas/ElementoListaDeNotas';
 import styles from './NavegacionDesktop.module.css';
 
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+
 export default function NavegacionDesktop() {
+	const state = useSelector((state: RootState) => state.nota);
+	const [searchInput, setSearchInput] = useState('');
+
+	function handleChangeSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
+		setSearchInput(e.target.value);
+	}
+
+	let resultadosFiltrados;
+
+	if (searchInput !== '') {
+		resultadosFiltrados = state.notas.filter(
+			(item) =>
+				item.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+				item.content.toLowerCase().includes(searchInput.toLowerCase())
+		);
+	}
+
+	console.log(state, 'el state del buscador');
+
 	return (
 		<section className={styles.section}>
 			<h3 className={`${styles.h3} text-preset-1`}>Search</h3>
 			<div className={styles.buscador}>
 				<img src={searchIcon} alt="" className={styles.lupa} />
-				<input type="text" className={`${styles.input} text-preset-5`} />
+				<input
+					type="text"
+					className={`${styles.input} text-preset-5`}
+					onChange={handleChangeSearchInput}
+				/>
 			</div>
-			<span className={`${styles.span} text-preset-5`}>
-				All notes matching "Dev" are displayed below.
-			</span>
+			{searchInput !== '' && (
+				<span className={`${styles.span} text-preset-5`}>
+					All notes matching "{searchInput}" are displayed below.
+				</span>
+			)}
 			<div className={styles.settingsIconDiv}>
 				<img
 					src={settingsIcon}
@@ -22,16 +51,10 @@ export default function NavegacionDesktop() {
 				/>
 			</div>
 			<div className={styles.listaDeNotas}>
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
-				<ElementoListaDeNotas />
+				{resultadosFiltrados &&
+					resultadosFiltrados.map((nota) => {
+						return <ElementoListaDeNotas nota={nota} />;
+					})}
 			</div>
 		</section>
 	);
