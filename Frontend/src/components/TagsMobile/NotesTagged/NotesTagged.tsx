ab20796d1186247acb79ebcb5f592735de275a32
RuxCastillo <rubenldc1412@gmail.com>
2025-04-07 18:38:08 -0600

    notestagged y los tag en mobile ya funcionan bien

--- Frontend/src/components/TagsMobile/NotesTagged/NotesTagged.tsx
+++ Frontend/src/components/TagsMobile/NotesTagged/NotesTagged.tsx
@@ -2,16 +2,27 @@ import ElementoListaDeNotas from '../../ListaDeNotas/ElementoListaDeNotas/Elemen
 import arrowLeftIcon from '../../../assets/images/icon-arrow-left.svg';
 import styles from './NotesTagged.module.css';
 import CirculoNuevaNota from '../../CirculoNuevaNota/CirculoNuevaNota';
-import { dispatch } from '@reduxjs/toolkit';
-import { AppDispatch } from '../../../store/store';
-import { useDispatch } from 'react-redux';
+import { AppDispatch, RootState } from '../../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
 import { tagSeleccionado } from '../../../store/reducer';
+import { Nota } from '../../../store/reducer';
 
 export default function NotesTagged() {
+	const state = useSelector((state: RootState) => state.nota);
 	const dispatch: AppDispatch = useDispatch();
 
 	function handleClickGoBackNotesTagged() {
 		dispatch(tagSeleccionado(null));
+	}
+
+	let filtrados: Nota[] = [];
+	if (state.tagSeleccionado) {
+		const tagSeleccionado = state.tagSeleccionado.toLowerCase();
+		filtrados = state.todasLasNotas.filter((item) => {
+			if (Array.isArray(item.tags)) {
+				return item.tags.some((tag) => tag.toLowerCase() === tagSeleccionado);
+			}
+		});
 	}
 
 	return (
@@ -21,19 +32,16 @@ export default function NotesTagged() {
 				<span className="text-preset-5">Go Back</span>
 			</div>
 			<h3 className={`${styles.h3} text-preset-1`}>
-				Notes Tagged: <span className={styles.h3Span}>Dev</span>
+				Notes Tagged:{' '}
+				<span className={styles.h3Span}>{state.tagSeleccionado}</span>
 			</h3>
 			<p className={`${styles.p} text-preset-5`}>
-				All notes with the "Dev" tag are shown here.
+				All notes with the "{state.tagSeleccionado}" tag are shown here.
 			</p>
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
-			<ElementoListaDeNotas />
+			{filtrados &&
+				filtrados.map((ele) => {
+					return <ElementoListaDeNotas nota={ele} />;
+				})}
 			<CirculoNuevaNota />
 		</section>
 	);
